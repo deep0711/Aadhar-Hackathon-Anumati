080449847a4402077a2a64baa7b18c71b840442d
deeparmyschool456 <deeparmyschool456@gmail.com>
2021-10-23 16:09:41 +0530

    REST APIs and Database Schema

---
+++ Anumati-Server/models/address_model.js
@@ -0,0 +1,49 @@
+const { Sequelize,sequelize, Op, Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize,DataTypes) => {
+    const Address = sequelize.define("Address",{
+        ConsentID : {
+            type : DataTypes.UUID,
+            allowNull : false,
+            validate : {
+                notEmpty : true
+            },
+            unique : true
+        },
+        GaurdianName : {
+            type : DataTypes.STRING
+        },
+        GaurdianType : {
+            type : DataTypes.STRING
+        },
+        HouseNumber : {
+            type : DataTypes.STRING
+        },
+        StreetName : {
+            type : DataTypes.STRING
+        },
+        Landmark : {
+            type : DataTypes.STRING
+        },
+        Area : {
+            type : DataTypes.STRING
+        },
+        Village : {
+            type : DataTypes.STRING
+        },
+        District : {
+            type : DataTypes.STRING
+        },
+        PostOffice : {
+            type : DataTypes.STRING
+        },
+        State : {
+            type : DataTypes.STRING
+        },
+        PinCode : {
+            type : DataTypes.STRING
+        },
+    });
+
+    return Address;
+}
