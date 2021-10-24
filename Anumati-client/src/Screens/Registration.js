c7cef8da91496b0da3ed3cdd4e68f5dcad0aee86
deeparmyschool456 <deeparmyschool456@gmail.com>
2021-10-24 13:51:52 +0530

    EKYC API Integration

--- Anumati-client/src/Screens/Registration.js
+++ Anumati-client/src/Screens/Registration.js
@@ -1,37 +1,188 @@
-import React , { useState } from "react";
+import React , { useState,useEffect } from "react";
 import { View , Image , StyleSheet, Text } from "react-native";
 import Logo from '../assets/Aadhar-Color.png';
 import { Divider , Button , Input ,useToast } from "native-base";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { v4 as uuid_v4 } from 'uuid';
+import * as Notifications from 'expo-notifications';
+
 
 function RegistrationScreen( props ) {
 
     const [aadharNo , setAadharNo] = useState('');
+    const [captcha, setCaptcha] = useState('');
     const [isLoading , setIsLoading] = useState(false);
+    const [captchaTxnId, setCaptchaTxnId] = useState('');
+    const [txnId,setTxnId] = useState('');
+    const [otp,setOtp] = useState('');
+    const [otpSent,setOtpSent] = useState(false);
+    const [base64Icon,setIcon] = useState('https://reactnative.dev/img/tiny_logo.png');
 
     const toast = useToast();
+            
+    useEffect(() => {
+        async function fetchCaptcha(){
+            //Calling Captcha API for setting up captcha
+            setIsLoading(true);
+            try{
+                fetch('https://stage1.uidai.gov.in/unifiedAppAuthService/api/v2/get/captcha',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                    'Accept':'application/json'
+                },
+                body:JSON.stringify({
+                    "langCode": "en",
+                    "captchaLength": "5",
+                    "captchaType": "2"
+                })
+                }).then( response => response.json()
+                ).then(function(response) {
+
+                    if(response["statusCode"] == 200)
+                    {
+                        setCaptchaTxnId(response["captchaTxnId"]);
+                        setIcon('data:image/png;base64,'+response["captchaBase64String"]);
+                        setIsLoading(false);
+                    }else{
+                        console.log("Error 1:",response);
+                        setIsLoading(false);
+                    }
+                }).catch( err => console.log(err));
+            }catch(err){
+                    console.log("Error occured while Generating Captcha :",err);
+                    setIsLoading(false);
+            }
+        }    
+        fetchCaptcha();
+    }, []);
 
     const handleSubmit = async() => {
         setIsLoading(true);
-            if (aadharNo.length === 12) {
-                try {
-                    await AsyncStorage.setItem('aAdharNumber', aadharNo);
-                } catch(e) {
-                    console.log(e);
-                }
-                props.navigation.navigate('CreatePIN');
-            } else {
-                console.log('Aadhar Number is 12 Digit Long!');
+
+        if (aadharNo.length === 12) {
+            try {
+                await AsyncStorage.setItem('aAdharNumber', aadharNo);
+
+                fetch('https://stage1.uidai.gov.in/unifiedAppAuthService/api/v2/generate/aadhaar/otp',{
+                    method:'POST',
+                    headers:{
+                        'Content-Type':'application/json',
+                        'x-request-id':uuid_v4(),
+                        'appid':"MYAADHAAR",
+                        'Accept-Language':'en_in'
+                    },
+                    body:JSON.stringify({
+                        "uidNumber": aadharNo,
+                        "captchaTxnId":captchaTxnId,
+                        "captchaValue":captcha,
+                        "transactionId":'MYAADHAAR:'+uuid_v4()
+                    })
+                }).then(response => response.json()
+                ).then(function(response){
+                    
+                    if(response["status"] == "Success")
+                    {
+                        toast.show({
+                            title: "OTP Sent to your Mobile",
+                            status: "success",
+                            duration: 3000,
+                            variant: "outline-light"
+                        });
+                        setOtpSent(true);
+                        setTxnId(response["txnId"]);
+                    }else if(response["type"] == "UNABLE_TO_REACH_AUTH_SERVICE_ERROR"){
+                        toast.show({
+                            title: "Entered Aadhar does not exist",
+                            status: "error",
+                            duration: 3000,
+                            variant: "outline-light"
+                        });
+                    }else{
+                        toast.show({
+                            title: "Captcha is Wrong",
+                            status: "error",
+                            duration: 3000,
+                            variant: "outline-light"
+                        });
+                    }
+
+                    setIsLoading(false);
+            }).catch(err => {console.log(err);setIsLoading(false);});
+            } catch(e) {
+                setIsLoading(false);
+                console.log(e);
+            }
+        } else {
+            console.log('Aadhar Number must be 12 Digit Long!');
+            setIsLoading(false);
+
+            toast.show({
+                title: "Wrong Input",
+                status: "error",
+                description: "Aadhar Number must be 12 Digit Long!  ",
+                duration: 5000,
+                variant: "outline-light"
+            });
+        }
+    }
+
+    const OTPSubmit = async () =>{
+        try{
+        setIsLoading(true);
+
+        fetch('https://stage1.uidai.gov.in/eAadhaarService/api/downloadOfflineEkyc',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                "txnNumber": txnId,
+                "otp":otp,
+                "shareCode":2469,
+                "uid":aadharNo
+            })
+        }).then(response => response.json()
+        ).then(async function(response){
+
+            if(response["status"] == "Success"){
+                //Data Fetch Successful and store the data in encrypted manner in Async storage-to be done
+                toast.show({
+                    title: "Registration Succesful",
+                    status: "success",
+                    duration: 3000,
+                    variant: "outline-light"
+                });
                 setIsLoading(false);
 
+                //Storing Token for Push Notification
+                const token = (await Notifications.getExpoPushTokenAsync()).data;
+                const response = await fetch('http://localhost:8000/anumati-server/store-token',{
+                    method:'POST',
+                    body:JSON.stringify({
+                        "Aadhar" :aadharNo,
+                        "Token" : token 
+                    })
+                })
+                
+                if(typeof response.error == 'undefined')
+                {
+                    console.log("Push Token Stored successfully");
+                }
+                
+                props.navigation.navigate('CreatePIN');
+            }else{
                 toast.show({
-                    title: "Wrong Input",
+                    title: "Wrong OTP",
                     status: "error",
-                    description: "Aadhar Number is 12 Digit Long!  ",
+                    description: "Try Again",
                     duration: 5000,
                     variant: "outline-light"
                 });
             }
+            setIsLoading(false);
+        }).catch(err => {console.log(err);setIsLoading(false);})
+    }catch(err){ console.log(err)};
     }
 
     return(
@@ -49,14 +200,37 @@ function RegistrationScreen( props ) {
                     keyboardType="number-pad"
                     onChangeText={text => setAadharNo(text)}
                 />
-                <Button 
+                <Image style={{width: '100%', height: 90,marginTop:20,marginBottom:20}} source={{uri: base64Icon}}/>
+                <Input 
+                    variant="filled" 
+                    placeholder="Enter Captcha"
+                    onChangeText={text => setCaptcha(text)}
+                />
+                
+                {otpSent ?
+                    <>
+                    <Input 
+                    variant="filled" 
+                    placeholder="Enter OTP sent to your mobile"
+                    keyboardType="number-pad"
+                    onChangeText={text => setOtp(text)}
+                    />
+                    <Button 
                     isLoading={isLoading} 
                     isLoadingText="Submitting" 
                     style={{marginTop: 15 , marginLeft: 15 , marginRight: 15}}
-                    onPress={() => handleSubmit()}
-                >
+                    onPress={() => OTPSubmit()}
+                    >
+                    Submit OTP
+                    </Button></>: 
+                    <Button 
+                        isLoading={isLoading} 
+                        isLoadingText="Submitting" 
+                        style={{marginTop: 15 , marginLeft: 15 , marginRight: 15}}
+                        onPress={() => handleSubmit()}
+                    >
                     Submit
-                </Button>
+                </Button>}
             </View>
         </View>
     )
